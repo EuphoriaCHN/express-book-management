1db5d989bc1e3476e97c439f84f2b9ca9f568f87
EuphoriaCHN <879969355@qq.com>
2020-02-09 20:11:00 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * 图书管理系统 - 入口文件
+ */
+
+const express = require('express');
+const template = require('art-template');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
