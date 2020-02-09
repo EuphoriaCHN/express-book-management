fceeafb0bbf483f1ccb55d520b53c56e77656a9f
EuphoriaCHN <879969355@qq.com>
2020-02-09 20:13:24 +0800

    设置Express模板引擎

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /**
  * 图书管理系统 - 入口文件
  */
+const path = require('path');
 
 const express = require('express');
 const template = require('art-template');
@@ -8,4 +9,10 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// 设置模板引擎
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'art');
+
+// 使 Express 兼容 Art-template 引擎
+app.engine('art', require('express-art-template'));
 
