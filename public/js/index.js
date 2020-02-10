ad8d4bb105881d80b9cf1763d883a1376203ca96
EuphoriaCHN <879969355@qq.com>
2020-02-10 18:09:38 +0800

    完成添加图书结构与动作

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,44 @@
 ((window, undefined) => {
-    $(function () {
-        // 渲染列表数据
-        $.ajax({
-            type: 'get',
-            url: '/books',
-            dataType: 'json',
-            success(res) {
-                let html = template('indexTpl', {
-                    list: res
-                });
-                $('#dataList').html(html);
-            }
+    function initList() {
+        $(function () {
+            // 渲染列表数据
+            $.ajax({
+                type: 'get',
+                url: '/books',
+                dataType: 'json',
+                success(res) {
+                    let html = template('indexTpl', {
+                        list: res
+                    });
+                    $('#dataList').html(html);
+                }
+            });
+        });
+    }
+
+    initList();
+
+    // 添加图书信息
+    $('#addBook').on('click', function () {
+        const form = $('#addBookForm');
+        const dialog = new MarkBox(600, 400, '添加图书', form.get(0));
+        dialog.init();
+
+        $('#addBookSubmit').on('click', function () {
+            $.ajax({
+                type: 'post',
+                url: '/books/book',
+                data: form.serialize(),
+                dataType: 'json',
+                success(res) {
+                    if (res.flag === '1') {
+                        dialog.close();
+                        initList();
+                    } else {
+                        console.error('Server Error');
+                    }
+                }
+            });
         });
     });
 })(window, undefined);
